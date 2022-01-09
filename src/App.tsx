90a90153e4a2ef7a9efee0d5e7a13b2fa8172bdd
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-01-09 15:07:26 +0900

    chore: update files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hi there!</div>;
+  return <div className="App">Hi there! This is Jiwan Jeon</div>;
 }
 
 export default App;
