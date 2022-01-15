bad5e55dd385d714a53bcb8ae6d06873652c0349
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-01-15 15:42:59 +0900

    Edit: Add comments for checking Github Action

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hi there! This is Jiwan Jeon</div>;
+  return (
+    <div className="App">Hi there! This is Jiwan Jeon Master Branch Check</div>
+  );
 }
 
 export default App;
