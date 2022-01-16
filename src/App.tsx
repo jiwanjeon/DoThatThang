43565347c0f0b016684f3688609070271a5b4470
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-01-16 12:30:58 +0900

    Add: Adding another line for checking Action Workflow

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       Hi there! This is Jiwan Jeon Master Branch Check
       <h1>Good Morning!</h1>
       <h2>CI/CD Workflow checking</h2>
+      <h3>Work or not?</h3>
     </div>
   );
 }
